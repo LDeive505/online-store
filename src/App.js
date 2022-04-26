6203cde6c821ba9612681a0a816a3db51bb5a3d4
Victor Vidal <victorvidal08@hotmail.com>
2022-04-26 16:01:57 -0300

    req 1 finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* getCategories() */}
+        {/* getProductsFromCategoryAndQuery('MLB5672', 'retrovisor') */}
+        teste
+      </div>
+    );
+  }
 }
-
-export default App;
