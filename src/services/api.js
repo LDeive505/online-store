22f89aea4799a31cc445be6cf636de83c7340ecc
Sandoerton <sandoerton@gmail.com>
2022-04-27 19:36:43 -0300

    Enviando requisito 6

--- src/services/api.js
+++ src/services/api.js
@@ -21,6 +21,17 @@ export async function getProducts(query) {
   }
 }
 
+export async function getProductsFromCategory(query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${query}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    return (error);
+  }
+}
+
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
