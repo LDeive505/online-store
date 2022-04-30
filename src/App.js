064d170c6cda873fed9e24f13e9663ef2fc2dd3f
LDeive505 <luisdaviab@gmail.com>
2022-04-30 15:43:44 -0300

    Alterações para requisito 9 + componente NotFound

--- src/App.js
+++ src/App.js
@@ -6,14 +6,52 @@ import ShoppingCart from './pages/ShoppingCart';
 import ProductDetails from './pages/ProductDetails';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cart: [],
+    };
+  }
+
+  handleCart = (product) => {
+    const cartProduct = { ...product, quantity: 1 };
+    this.setState((prevState) => ({
+      cart: [...prevState.cart, cartProduct],
+    }));
+  };
+
   render() {
+    const { cart } = this.state;
     return (
       <div>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={ Search } />
-            <Route path="/ShoppingCart" component={ ShoppingCart } />
-            <Route path="/productDetails" component={ ProductDetails } />
+            <Route
+              exact
+              path="/"
+              render={ (props) => (
+                <Search
+                  { ...props }
+                  handleCart={ this.handleCart }
+                />) }
+            />
+            <Route
+              path="/shoppingCart"
+              render={ (props) => (
+                <ShoppingCart
+                  { ...props }
+                  cart={ cart }
+                />) }
+            />
+            <Route
+              path="/productDetails/:id"
+              render={ (props) => (
+                <ProductDetails
+                  { ...props }
+                  handleCart={ this.handleCart }
+                />) }
+            />
+            <Route component={ NotFound } />
           </Switch>
         </BrowserRouter>
       </div>
